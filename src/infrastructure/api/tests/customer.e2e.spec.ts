37097691fa928ba09eb68a0f651fc1edb9e572d3
wilfoz <wil_foz@yahoo.com.br>
2022-05-24 20:37:33 -0300

    feat: :sparkles: Presenter

--- src/infrastructure/api/tests/customer.e2e.spec.ts
+++ src/infrastructure/api/tests/customer.e2e.spec.ts
@@ -35,7 +35,7 @@ describe('E2E test for customer', () => {
             .send({
                 name: 'John Deu'
             });
-        
+
         expect(response.status).toBe(500);
     });
 
@@ -76,5 +76,21 @@ describe('E2E test for customer', () => {
         const customer2 = listResponse.body.customers[1];
         expect(customer2.name).toBe('Tony');
         expect(customer2.address.street).toBe('Street1');
+
+        const listResponseXML = await request(app)
+            .get('/customer')
+            .set('Accept', 'application/xml')
+            .send();
+
+        expect(listResponseXML.status).toBe(200);
+        expect(listResponseXML.text).toContain('<?xml version="1.0" encoding="UTF-8"?>');
+        expect(listResponseXML.text).toContain('<customers>');
+        expect(listResponseXML.text).toContain('<customer>');
+        expect(listResponseXML.text).toContain('<name>John</name>');
+        expect(listResponseXML.text).toContain('<address>');
+        expect(listResponseXML.text).toContain('<street>Street</street>');
+        expect(listResponseXML.text).toContain('<number>123</number>');
+        expect(listResponseXML.text).toContain('<zip>123</zip>');
+        expect(listResponseXML.text).toContain('<city>City</city>');
     });
 });
